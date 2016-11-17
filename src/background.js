608959bc8eeefe5db02bdc018b13c402b73f8f7e
Ramachandra Junior <d.ajaykumar.017@gmail.com>
2016-11-17 20:37:02 +0530

    Initial

--- src/background.js
+++ src/background.js
@@ -1,32 +1,29 @@
 /*
- This script does all the calculation stuff.
- This script can only listen to messages
- and send response.
+ *  This script does all the calculations.
+ *  This script can only listen to messages
+ * and send response.
 */
 
 
 
 // ==========
-// ==========
 // 1 - PASSWORD CREATOR.
 // ==========
-// ==========
 
-
-// ==========
 // MAKING PASSWORD
-// ==========
 
-// This function returns random characters.
+/*  This function returns random characters.
+ *  @params {String} charType  This is used to specify the character type to select.
+ */
+ 
 var randChar = (function (charType) {
 
-	// CHARACTER DATA
+	// character data
 	var alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],		
 		numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
 		alphaNumers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
 		symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"],
 		allChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "*", "(", ")", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "#", "$", "%", "6", "7", "8", "9", "!", "@", "^", "&"];
-
 	// Will hold the selected charset.
 	var selectedCharset = [];
 
@@ -53,12 +50,16 @@ var randChar = (function (charType) {
 
 	// Choose a random number.
 	var randNum = Math.floor(Math.random() * selectedCharset.length);
-
 	// Return that random character.
 	return (selectedCharset[randNum]);
 });
 
-// Create a password.
+
+/*  Creates a password.
+ *  @params {Number} len       This specifies the length of the password.
+ *  @params {String} charType  This is used to specify the character type to select.
+ */
+
 var makePassword = (function (len, charType) {
 
 	// Set password.len to 10.
@@ -75,9 +76,10 @@ var makePassword = (function (len, charType) {
 
 
 
-// ==========
-// SEND PASSWORD
-// ==========
+/*  SEND PASSWORD
+ *  @param {Object} sendRes Method to send response to the requester.
+ *  We will be carrying this object all script long to avoid confusion.
+*/
 
 // Calling make password.
 var init = function(sendRes) {
@@ -89,6 +91,8 @@ var init = function(sendRes) {
 	// Send data to popup.js
 	sendRes(msgObj);
 
+	// Injecting our own scripts, these will display password in a
+	// div directly appended to page body.
 	// Get tab details.
 	chrome.tabs.query(tabsQuery, function(tabs) {
 		var activeTab = tabs[0];
@@ -111,15 +115,10 @@ var init = function(sendRes) {
 	
 
 // ==========
-// ==========
 // 0 - DEAL WIH MESSAGES.
 // ==========
-// ==========
 
-
-// ==========
 // GLOBALS
-// ==========
 
 // Message structure.
 var msgObj = {
@@ -131,15 +130,12 @@ var msgObj = {
 	error: {
 		value: null
 	}
-}
-
-
+};
 // Password object.
 var password = {
 	value: "",
 	len: 0
 };
-
 // Declare an object, this will be used to query,
 // present tab id.
 tabsQuery = {
@@ -149,54 +145,51 @@ tabsQuery = {
 
 
 
-// ==========
-// GET TAB
-// ==========
 
-// Shifted
-
-
-
-// ==========
-// ADD EVENT LISTENER
-// ==========
-
+/* MESSAGE EVENT LISTENER
+ *  @param {Object} req     Holds the structured message from any script.
+ *  @param {Object} n       Request sender data. But we'll use our own structure.
+ *  @param {Object} sendRes Method to send response to the requester.
+ */
 // Event listener for messages.
 chrome.runtime.onMessage.addListener(function(req, n, sendRes) {
-	// ======
-	// IF SRC IS POPUP JS.
-	// ======
-	if (req.src === "popup.js") {
-		// Looking at message.
-		switch (req.msg.value) {
-			// If popup says - INIT
-			case "init":
-				init(sendRes);
-				break;
-			case "get-existing-pass":
-				// Fill in password in data property.
-				msgObj.msg.data = password.value;
-				sendRes(msgObj);
-				break;
+	try {
+
+		// IF SRC IS POPUP JS.
+		if (req.src === "popup.js") {
+			// Looking at message.
+			switch (req.msg.value) {
+				// If popup says - INIT
+				case "init":
+					init(sendRes);
+					break;
+				case "get-existing-pass":
+					// Fill in password in data property.
+					msgObj.msg.data = password.value;
+					sendRes(msgObj);
+					break;
+			}			
+		} 
+		// If src is password.
+		else if (req.src === "password.js") {
+			// Looking at message.
+			switch (req.msg.value) {
+				// If popup says - need 
+				case "need-pass":
+					// Fill in password in data property.
+					msgObj.msg.data = password.value;
+					sendRes(msgObj);
+					break;
+			}
 		}
-		
-	} 
-	// ======
-	// IF SRC IS PASSWORD JS.
-	// ======
-	else if (req.src === "password.js") {
-		// Looking at message.
-		switch (req.msg.value) {
-			// If popup says - INIT
-			case "need-pass":
-				// Fill in password in data property.
-				msgObj.msg.data = password.value;
-				sendRes(msgObj);
-				break;
+		// If not any of the above.
+		else {
+			throw "Password request from an unknown source.";
 		}
+
 	}
-	else {
-		console.log(JSON.stringify(msgObj));
+	catch(err) {
+		console.warn("Error: " + err);
 	}
 
 });
